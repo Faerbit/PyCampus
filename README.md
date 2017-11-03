83ffaf81e4c7f9b0f9af2bf2d42cbf872a742427
Faerbit <faerbit@gmail.com>
2017-11-03 03:03:25 +0100

    Added RWTH Aachen as well as an index page.

--- README.md
+++ README.md
@@ -1,14 +1,17 @@
 # Campus ICS Feed
 
-This is a simple web server which serves the ICS Calendar from the FH Aachen
-Campus.
+This is a simple web server which serves the ICS schedule from
+Campus Office for RWTH Aachen and FH Aachen.
 
-##D ependencies
-This scripts depends on **Python 3**, the **requests** and the
-**Flask** package from python.
+## Dependencies
+
+* Python 3
+* requests
+* Flask
+
 
 ## Usage
-The script provides a WSGI interface called 'app'. For deploying such a
+The script provides a WSGI interface called 'app'. For deploying such an
 interface please see other resources. Your credentials are supplied via basic
 HTTP auth.
 
--- server.py
+++ server.py
@@ -1,12 +1,10 @@
 #!/usr/bin/env python3
 
-import configparser
-
 import requests
 import os
 from datetime import datetime, timedelta
 
-from flask import Flask, Response, abort, request
+from flask import Flask, Response, abort, request, render_template
 
 def authenticate():
     """ Sends a 401 response that enables basic auth"""
@@ -16,11 +14,24 @@ def authenticate():
 
 app = Flask(__name__)
 app.config["PROPAGATE_EXCEPTIONS"] = True
-BASE_URL = "https://www.campusoffice.fh-aachen.de/"
 
 @app.route("/")
 def index():
-    """Serves the ICS file."""
+    """Return index with basic information"""
+    return render_template("index.html")
+
+@app.route("/rwth")
+def rwth():
+    """Serve ICS file for RWTH Aachen"""
+    return ics("https://www.campus.rwth-aachen.de/office/")
+
+@app.route("/fhac")
+def fhac():
+    """Server ICS file for FH Aachen"""
+    return ics("https://www.campusoffice.fh-aachen.de/")
+
+def ics(base_url):
+    """Authenticate and serve the ICS file"""
 
     # use HTTP basic auth for auth with campus
     auth = request.authorization
@@ -33,7 +44,7 @@ def index():
         return authenticate()
     session = requests.Session()
     data = { "u": campus_user, "p": campus_password }
-    login_response = session.post(BASE_URL + "views/campus/search.asp",
+    login_response = session.post(base_url + "views/campus/search.asp",
             data=data, timeout = 20)
     if "fehlgeschlagen" in login_response.text:
         abort(403)
@@ -41,7 +52,7 @@ def index():
             timedelta(days=-180)).strftime("%d.%m.%Y")
     end_date    =   (datetime.now() 
             + timedelta(days=+180)).strftime("%d.%m.%Y")
-    ics_file_response = session.get(BASE_URL +
+    ics_file_response = session.get(base_url +
             "views/calendar/iCalExport.asp?startdt={}&enddt={}%2023:59:59"
             .format(start_date, end_date), timeout = 20)
     if (ics_file_response.status_code == 200
